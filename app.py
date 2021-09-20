86187179598eb32d7aeb202f120a37c9037bf41e
Nicolas García <nicolas.garcia@compucad.com.mx>
2021-09-20 18:11:11 -0500

    Open csv function

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask
+import csv
 app = Flask(__name__)
+
+name = 'Nico'
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Welcome! "+name
+
+@app.route("/open-csv")
+def openCsv():
+    data = ""
+    with open('GetSubscriptions.csv',encoding ="utf8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                #print(f'\t{row}')
+                data+= f'\t{row}'
+                line_count += 1
+        print(f'Processed {line_count} lines.')
+    return data
+
